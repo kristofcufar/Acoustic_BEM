5990fddcd1cf6b9cc150ed9f6ecaa02eb31e8f5c
Krištof Čufar <kristof.cufar@gmail.com>
2025-09-26 14:30:29 +0200

    Init change.

--- acoustic_BEM/__init__.py
+++ acoustic_BEM/__init__.py
@@ -4,7 +4,13 @@ Boundary Element Method for Exterior Acoustic problems
 
 __version__ = "0.0.1"
 from .mesh import Mesh
+from .integrators import (ElementIntegratorCollocation, 
+                          ElementIntegratorGalerkin)
+from .matrix_assembly import (CollocationAssembler, 
+                              GalerkinAssembler)
 from .solve import BEMSolver
 
-__all__ = ["Mesh", "BEMSolver"]
+__all__ = ["Mesh", "BEMSolver",
+            "CollocationAssembler", "GalerkinAssembler", 
+            "ElementIntegratorCollocation", "ElementIntegratorGalerkin"]
 __version__ = "0.0.0"
