46a5d03e373ddd36413960f84f1edfa5e730ec34
kristofcufar <kristof.cufar@gmail.com>
2026-01-14 20:39:43 +0100

    Started discont. elements implementation.

--- acoustic_BEM/__init__.py
+++ acoustic_BEM/__init__.py
@@ -5,6 +5,7 @@ Boundary Element Method for Exterior Acoustic problems
 __version__ = "0.0.1"
 from .geometry import Body, Field, box_mesh
 from .mesh import Mesh
+from .elements import (ContinuousP1Mesh, DiscontinuousP1Mesh)
 from .integrators import (ElementIntegratorCollocation)
 from .matrix_assembly import (CollocationAssembler)
 from .solve import BEMSolver
@@ -15,5 +16,7 @@ from .kernels import (r_vec, G, dG_dr,
 
 __all__ = ["Mesh", "BEMSolver",
             "CollocationAssembler", 
-            "ElementIntegratorCollocation"]
+            "ElementIntegratorCollocation",
+            "ContinuousP1Mesh", "DiscontinuousP1Mesh",
+            "Body", "Field", "box_mesh",]
 __version__ = "0.0.0"
